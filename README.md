58410ca60cf4c2585c6fac90d9942897f13e158b
Ivan Leichtling <ivanlei@yelp.com>
2013-07-31 11:21:54 -0700

    Initial code commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ puppet-wirelesstools
 ====================
 
 A puppet module for installing a set of packages for wireless pentesting
+
+Example
+-------
+```ruby
+class {'wireless_tools': }
+```
---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+
+class wireless_tools {
+  require python_tools
+  require hg
+
+  $packages = ['aircrack-ng', 'wireless-tools', 'iw', 'wireshark', 'net-tools']
+
+  package { $packages:
+    ensure => present,
+  }
+
+  python_tools::source_install { 'scapy':
+    repo_url => 'http://hg.secdev.org/scapy',
+    provider => 'hg',    
+  }
+}
